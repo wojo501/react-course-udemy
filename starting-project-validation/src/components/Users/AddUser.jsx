28c83dcfcfc6fed0bca7c0f7bdd7b704a30a3bf9
wojo501 <108939761+wojo501@users.noreply.github.com>
2022-09-16 12:40:38 +0200

    project fully done

--- starting-project-validation/src/components/Users/AddUser.jsx
+++ starting-project-validation/src/components/Users/AddUser.jsx
@@ -1,32 +1,28 @@
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUsername] = useState("");
-    const [enteredAge, setEnteredAge] = useState("");
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
     const [errorOccured, setErrorOccured] = useState(false);
 
     const addUsertHandler = (event) => {
         event.preventDefault();
-        if (enteredUsername.trim().length === 0 || +enteredAge < 1 || enteredAge.trim().length === 0) {
+        console.log("Refs: ", nameInputRef, ageInputRef);
+        const enteredName = nameInputRef.current.value;
+        const enteredUserAge = ageInputRef.current.value;
+        if (enteredName.trim().length === 0 || +enteredUserAge < 1 || enteredUserAge.trim().length === 0) {
             setErrorOccured(true);
             return;
         }
-        console.log("New user data: ", enteredAge, enteredUsername);
-        props.onAddUser(enteredUsername, enteredAge);
-        setEnteredUsername("");
-        setEnteredAge("")
-    }
-
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value);
-    }
-
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
+        props.onAddUser(enteredName, enteredUserAge);
+        nameInputRef.current.value = "";
+        ageInputRef.current.value = "";
     }
 
     const errorHandler = () => {
@@ -34,18 +30,26 @@ const AddUser = (props) => {
     }
 
     return (
-        <div>
+        <Wrapper>
             {errorOccured && <ErrorModal onConfirm={errorHandler} title="An error occured" message="User data are invalid" />}
             <Card className={classes.input}>
                 <form onSubmit={addUsertHandler} action="">
                     <label htmlFor="username">Username</label>
-                    <input id="username" value={enteredUsername} type="text" onChange={usernameChangeHandler} />
+                    <input
+                        id="username"
+                        type="text"
+                        ref={nameInputRef}
+                    />
                     <label htmlFor="age">Age (Years)</label>
-                    <input id="age" value={enteredAge} type="number" onChange={ageChangeHandler} />
+                    <input
+                        id="age"
+                        type="number"
+                        ref={ageInputRef}
+                    />
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
 
     )
 }
